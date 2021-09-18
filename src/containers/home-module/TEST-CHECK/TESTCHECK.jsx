f3d3ec451e715a2d1f67bcd5ddfc0278e6a25934
Tran Phuong <tranphuongdev31397@gmail.com>
2021-09-18 17:33:47 +0700

    'test'

--- src/containers/home-module/TEST-CHECK/TESTCHECK.jsx
+++ src/containers/home-module/TEST-CHECK/TESTCHECK.jsx
@@ -4,7 +4,7 @@ export default class TESTCHECK extends Component {
     render() {
         return (
             <div>
-                TESTCHECK
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate sunt sequi modi! Pariatur voluptas possimus earum vero veniam delectus? Ratione deserunt alias nisi libero ipsam quaerat exercitationem amet architecto itaque.
             </div>
         )
     }
