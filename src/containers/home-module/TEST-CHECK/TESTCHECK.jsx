67f3fc8a27fb6b646321291e4af361d30c42d0f8
Tran Phuong <tranphuongdev31397@gmail.com>
2021-09-18 17:33:01 +0700

    'test

---
+++ src/containers/home-module/TEST-CHECK/TESTCHECK.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TESTCHECK extends Component {
+    render() {
+        return (
+            <div>
+                TESTCHECK
+            </div>
+        )
+    }
+}
